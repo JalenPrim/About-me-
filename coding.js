f5227a02b6ee82cac583bfdacc096d41c6d419c7
JalenPrim <1265087@essextech.org>
2025-04-14 12:49:05 -0400

    Update coding.js

--- coding.js
+++ coding.js
@@ -1,8 +1,34 @@
-const container = document.getElementById("_____");
+const container = document.getElementById("list-container");
 
 for (let i = 1; i <= 5; i++) {
   const p = document.createElement("p");
-  p.textContent = "This is item #" + _____;
+  p.textContent = "This is item #" + i;
   container.appendChild(p);
 }
+
+let j = 1;
+while (j <= 5) {
+  const p = document.createElement("p");
+  p.textContent = "While loop item #" + j;
+  container.appendChild(p);
+  j++;
+}
+
+
+for (let i = 1; i <= 20; i++ ) {
+  const p = document.createElement("p");
+  p.textContent = i;
+
+  if (i _____) {
+
+  p.style.color = "blue";
+  } else {
+    p.style.color = "red";
+  }
+
+  container.appendChild(p);
+}
+
+
+
 
