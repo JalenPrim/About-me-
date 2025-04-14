9b88babdbd54ad9c692ec580bd1b89c462626294
JalenPrim <1265087@essextech.org>
2025-04-14 12:33:06 -0400

    Update coding.js

--- coding.js
+++ coding.js
@@ -1,1 +1,8 @@
+const container = document.getElementById("_____");
+
+for (let i = 1; i <= 5; i++) {
+  const p = document.createElement("p");
+  p.textContent = "This is item #" + _____;
+  container.appendChild(p);
+}
 
